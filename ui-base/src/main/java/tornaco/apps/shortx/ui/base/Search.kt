52b7baf3daa91aa86d1fae35bf2121cadbfe2a35
Tornaco <Tornaco@163.com>
2024-06-30 14:00:53 +0800

    Auto request focus

--- ui-base/src/main/java/tornaco/apps/shortx/ui/base/Search.kt
+++ ui-base/src/main/java/tornaco/apps/shortx/ui/base/Search.kt
@@ -10,12 +10,15 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import tornaco.apps.shortx.core.res.Remix
@@ -53,8 +56,10 @@ fun SearchBar(
 fun SearchTextField(searchBarState: SearchBarState, onSearchClick: () -> Unit) {
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
     ShortXTextField(modifier = Modifier
-        .fillMaxWidth(),
+        .fillMaxWidth()
+        .focusRequester(focusRequester),
         value = searchBarState.keyword,
         label = if (searchBarState.count > 0) {
             {
@@ -89,6 +94,10 @@ fun SearchTextField(searchBarState: SearchBarState, onSearchClick: () -> Unit) {
         onValueChange = {
             searchBarState.inputKeyword(it)
         })
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 }
 
 class SearchBarState(val count: Int, val keywordChanged: (String) -> Unit) {
